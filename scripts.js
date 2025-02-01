609405232fbd157aed1f27db832b7b0d3c8ac58b
Donovan Bezuidenhout <2353707@csfoy.ca>
2025-02-01 14:47:26 -0500

    premier

---
+++ scripts.js
@@ -0,0 +1,27 @@
+function openMainPage() {
+    window.location.href = 'main.html';
+}
+
+function openComplimentGenerator() {
+    window.location.href = 'compliment.html';
+}
+
+function openLoveGame() {
+    window.location.href = 'lovegame.html';
+}
+
+function showCallHours() {
+    window.location.href = 'callhours.html';
+}
+
+function generateLove() {
+    window.location.href = 'iloveyou.html'; // Modifiez cette ligne
+}
+
+function openStoryBook() {
+    window.location.href = 'storybook.html';
+}
+
+function openPuzzle() {
+    window.location.href = 'puzzle.html';
+}
